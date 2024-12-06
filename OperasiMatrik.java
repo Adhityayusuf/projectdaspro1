2d14f065bad32a22cd7a9a0424bcd95179d5e1e0
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-12-06 20:32:27 +0700

    p

--- OperasiMatrik.java
+++ OperasiMatrik.java
@@ -140,14 +140,16 @@ public class OperasiMatrik {
                 }
             }
         }
-        System.out.println("Hasil perkalian matriks:");
+        System.out.println("\nHasil perkalian matriks:");
+        System.out.println();
         for (int i = 0; i < barisA; i++) {
-            System.out.println("| ");
+            System.out.print("| ");
             for (int j = 0; j < kolomB; j++) {
                 System.out.print(hasilPerkalian[i][j] + " ");
             }
-            System.out.println("|");
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println();
     }
 }
